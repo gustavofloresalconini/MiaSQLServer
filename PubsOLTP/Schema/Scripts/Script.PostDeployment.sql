cc6bb0823a2d06855645ceca98a064a2b59acb0f
gaflores <gaflores@LPZ-P-252456.local>
2025-11-10 15:51:11 -0400

    1.2 Tarea: Modelo Entidad-Relacion

---
+++ PubsOLTP/Schema/Scripts/Script.PostDeployment.sql
@@ -0,0 +1,12 @@
+
+:r .\.dbo.authros.Data.sql
+:r .\.dbo.discounts.Data.sql
+:r .\.dbo.employee.Data.sql
+:r .\.dbo.jobs.Data.sql
+:r .\.dbo.pub_info.Data.sql
+:r .\.dbo.publishers.Data.sql
+:r .\.dbo.roysched.Data.sql
+:r .\.dbo.sales.Data.sql
+:r .\.dbo.stores.Data.sql
+:r .\.dbo.titleauthor.Data.sql
+:r .\.dbo.titles.Data.sql
